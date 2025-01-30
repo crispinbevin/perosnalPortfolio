22a3c27d228938904b3bf6aac1f57516cf288050
Crispin Bevin <crispinjithin@gmail.com>
2025-01-30 15:00:49 +0530

    navbar but not responsive

--- personal-portfolio/src/components/Navbar.jsx
+++ personal-portfolio/src/components/Navbar.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import Logo from '../assets/logo.png'
 import {Link} from 'react-router-dom'
+import './Navbar.css'
 
 function Navbar() {
   return (
     <div className="navigation-section">
-        <div className="nav-logo">
-            <img src={Logo} alt="" />
+        <div className="nav-wrapper">
+          <div className="nav-logo">
+              <img src={Logo} alt="" />
+          </div>
+          <ul className='nav-list'>
+              <Link to="/" className="nav-link"><li className='nav-item'>HOME</li></Link>
+              <Link className="nav-link"><li className='nav-item'>ABOUT</li></Link>
+              <Link className="nav-link"><li className='nav-item'>SERVICES</li></Link>
+              <Link className="nav-link"><li className='nav-item'>WORKS</li></Link>
+              <Link className="nav-link"><li className='nav-item'>CONTACT</li></Link>
+          </ul>
         </div>
-        <ul className='nav-list'>
-            <Link to="/" className="nav-link"><li className='nav-item'>Home</li></Link>
-            <Link className="nav-link"><li className='nav-item'>About</li></Link>
-            <Link className="nav-link"><li className='nav-item'>Services</li></Link>
-            <Link className="nav-link"><li className='nav-item'>Works</li></Link>
-            <Link className="nav-link"><li className='nav-item'>Contact</li></Link>
-        </ul>
     </div>
   )
 }
