010f81bc4050cf3adc1e44a111640a7c444081d5
Crispin Bevin <crispinjithin@gmail.com>
2025-01-31 11:05:31 +0530

    added icons and touched up home

--- personal-portfolio/src/Home.jsx
+++ personal-portfolio/src/Home.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar.jsx'
 import About from './About.jsx'
-import Services from './Services.jsx'
-
+import Services from './Services.jsx' 
+import 'react-social-icons'
+import 'react-social-icons/instagram'
+import 'react-social-icons/facebook'
+import 'react-social-icons/pinterest'
+import 'react-social-icons/twitter'
+import 'react-social-icons/behance'
+import { SocialIcon } from 'react-social-icons/component'
 function Home() {
   return (
     <>
@@ -11,10 +17,19 @@ function Home() {
       <div className="home-section">
         <div className="home-wrapper">
           <div className="home-subheading">WEB DEVELOPER & UX DESIGNER</div>
-          <div className="switching-text">TEXT ANIMATION
+          <div className="switching-text">
+            WEB DESIGN
           </div>
           <div className="home-text">I’m a Creative Designer and I make things <br />
           Turning your dreams into reality.</div>
+          <br />
+          <div className="socials-list">
+            <SocialIcon className="socials-icon" url='facebook.com' bgColor='rgb(0,0,0,0) ' fgColor='currentColor'/>
+            <SocialIcon className="socials-icon" url='twitter.com' bgColor='rgb(0,0,0,0)' fgColor='currentColor'/>
+            <SocialIcon className="socials-icon" url='instagram.com' bgColor='rgb(0,0,0,0)' fgColor='currentColor'/>
+            <SocialIcon className="socials-icon" url='pinterest.com' bgColor='rgb(0,0,0,0)' fgColor='currentColor'/>
+            <SocialIcon className="socials-icon" url='behance.com' bgColor='rgb(0,0,0,0)' fgColor='currentColour'/>
+          </div>
         </div>
       </div>
       <About />
